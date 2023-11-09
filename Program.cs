a315c9b9a9991e4c747c0db6d85926f55c26fce7
Da-Boi42 <147948548+Da-Boi42@users.noreply.github.com>
2023-11-09 13:54:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mine
+{
+    class Program
+    {
+        class Person {
+            int age;
+            
+            public void SayHi() {
+                Console.WriteLine("Hi");
+            }
+            static void Main(string[] args)
+            {
+                Person p1 = new Person();
+                p1.SayHi();
+            }
+        }
+    }
+}
